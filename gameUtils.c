d77d70445aa54da24f8b3f34f03bd59dfe9d6c35
Robert Winter <rdwinter3@gmail.com>
2019-03-08 21:19:35 -0600

    update

--- gameUtils.c
+++ gameUtils.c
@@ -82,7 +82,7 @@ Status getStatus(Move **board) {
   Status s = TIE;
 
   if( //rows
-      (board[0][1] == board[0][1] && board[0][1] == board[0][1] && board[0][0] == X) ||
+      (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] == X) || //top row for x wins
       (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] == X) ||
       (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] == X) ||
       //cols
